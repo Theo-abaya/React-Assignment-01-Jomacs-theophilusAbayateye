62f2fa3f95ae563c3f6ea9ba0b7f9353eae0e255
theo <abayateyet@gmail.com>
2025-04-28 13:37:56 +0000

    List of my favourite fruits

--- src/components/Test08.jsx
+++ src/components/Test08.jsx
@@ -6,7 +6,13 @@ Use JSX to render a <ul> list of three favorite fruits.
 
 function Test08() {
   return (
-    // Your code here
+    <div>
+      <ul>
+        <li>Banana</li>
+        <li>Watermelon</li>
+        <li>Orange</li>
+      </ul>
+    </div>
   );
 }
 
